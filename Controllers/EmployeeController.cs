4f26df479dea5034f806a88177a557a7dd5b3ff4
Tico Clark <tico-devs@protonmail.com>
2023-02-23 15:16:12 -0800

    employee Controller update

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -1,5 +1,7 @@
-﻿using CoreWebAPI.Repository;
+﻿using CoreWebAPI.Models;
+using CoreWebAPI.Repository;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CoreWebAPI.Controllers
@@ -10,9 +12,12 @@ namespace CoreWebAPI.Controllers
     {
         private readonly IEmployeeRepository _employee;
         private readonly IDepartmentRepository _department;
-        public EmployeeController(IEmployeeRepository employee, IDepartmentRepository department) {
+        private readonly IWebHostEnvironment  _env;
+        public EmployeeController(IEmployeeRepository employee, IDepartmentRepository department, IWebHostEnvironment env)
+        {
             _employee = employee ?? throw new ArgumentNullException(nameof(employee));
             _department = department ?? throw new ArgumentNullException(nameof(department));
+            _env = env;
         }
         [HttpGet]
         [Route("GetEmployee")]
@@ -25,6 +30,62 @@ namespace CoreWebAPI.Controllers
         public async Task<IActionResult> GetEmpByID(int id)
         {
             return Ok(await _employee.GetEmployeeByID(id));
+        }
+
+        [HttpPost]
+        [Route("AddEmployee")]
+        public async Task<IActionResult> Post(Employee emp)
+        {
+            var result = await _employee.InsertEmployee(emp);
+            if(result.EmployeeID == 0) { 
+            return StatusCode(StatusCodes.Status500InternalServerError, "Something Went Wrong");
+            }
+            return Ok("Added Successfully");
+        }
+        [HttpPut]
+        [Route("UpdateEmployee")]
+        public async Task<IActionResult> Put(Employee emp)
+        {
+           var result  =  await _employee.UpdateEmployee(emp);
+            return Ok($"Updated Successfully: {result}");
+        }
+        [HttpDelete]
+        [Route("DeleteEmployee")]
+        public JsonResult Delete(int id) { 
+        var result = _employee.DeleteEmployee(id);
+            return new JsonResult($"deleted Successfully : {result}");
+        }
+
+
+        [Route("SaveFile")]
+        [HttpPost]
+        public JsonResult SaveFile()
+        {
+            try
+            {
+
+                var httpRequest = Request.Form;
+                var postedFile = httpRequest.Files[0];
+                string filename = postedFile.FileName;
+                var physicalPath = _env.ContentRootPath + "/Photos/"+ filename;
+
+                using(var stream = new FileStream(physicalPath, FileMode.Create))
+                {
+                    stream.CopyTo(stream);
+                }
+                return new JsonResult(filename);
+            
+            }
+            catch (Exception) {
+                return new JsonResult("anonymous.png");
+            }
+        }
+
+        [HttpGet]
+        [Route("GetDepartment")]
+        public async Task <IActionResult> GetAllDepartmentNames()
+        {
+            return Ok(await _department.GetDepartment());
         }
     }
 }
